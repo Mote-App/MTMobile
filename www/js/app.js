f98d9a81f16e3c710ccfa7ae7bb9e9d668aa339f
Gibran Castillo <gibran-castillo@hotmail.com>
2015-12-29 00:03:02 -0500

    Replaced AWS EC2 with localhost

--- www/js/app.js
+++ www/js/app.js
@@ -31,9 +31,9 @@ angular.module('Mote', ['ionic',
 
 .run(function($ionicPlatform, $rootScope, $filter, $window, $state, $localstorage, OpenFB) {
 
-	//AWS http://54.149.27.205
+	// AWS EC2 URL http://54.149.27.205     Account was closed 28th December 2015
 	
-	$rootScope.clhost = "http://54.149.27.205";
+	$rootScope.clhost = "http://127.0.0.1";  // localhost URL
 	$rootScope.clport = ":8080";
 	$rootScope.lstTag = null;
 	$rootScope.colleges = null;
