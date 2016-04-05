4697aab4f6653ed68413e7832ec78ade493a8aa8
Sanjay Sao <sanjaysao@gmail.com>
2016-04-04 21:01:30 -0400

    updated fix for missing statusbar

--- www/js/app.js
+++ www/js/app.js
@@ -32,6 +32,8 @@ angular.module('Mote', ['ionic',
 
 .run(function($ionicPlatform, $rootScope, $filter, $window, $state, $localstorage) {
 
+	//$cordovaStatusbar.hide();
+	
 	// AWS EC2 URL http://54.149.27.205     Account was closed 28th December 2015
 	// New AWS EC2 URL http://54.200.159.155
 	$rootScope.clhost = "http://54.200.159.155";  // localhost URL
